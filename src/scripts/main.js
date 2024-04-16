99139f60257741e28d3e03cc1e432a5971665235
sem9508 <sem40911@gmail.com>
2024-04-16 18:35:11 +0200

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+require('dotenv').config()
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      title: 'Flash Cards',
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+ 
+    win.setMenu(null)
+    if (process.env.OPEN_DEV_TOOLS == 'true'){
+      win.webContents.openDevTools()
+    }
+    
+    win.loadFile(path.join(__dirname, '../html/index.html'))
+}
+
+app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
